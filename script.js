73d01fd44513a804990aa392966362dbebe6d9aa
Tesskortekaas <150714663+Tesskortekaas@users.noreply.github.com>
2023-12-11 17:00:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var openButton = document.querySelector("header nav ul li > button");
 var closeButton = document.querySelector("header nav ul:nth-of-type(2) > button");
+
  
 openButton.onclick = openMenu;
 closeButton.onclick = closeMenu;
+
  
 function openMenu() {  
   var deNav = document.querySelector("header nav");
@@ -22,5 +24,24 @@ function handleKeydown(event) {
     deNav.classList.remove("toonMenu");
   }
 }
-
 // elaine heeft mij hier heel veel bij geholpen want ik kwam er in mijn eentje niet uit.
+
+var hartIcoon = document.querySelector("main article img");
+
+hartIcoon.onclick = hartRood;
+
+function hartRood() {
+  if ( hartIcoon.src == "img/harticoon.png" ) {
+    
+     hartIcoon.src == "img/harticoon2.png";
+  }
+  else {
+    hartIcoon.src == "img/harticoon.png";
+  }
+}
+
+ 
+
+//#endregion #
+
+// PaymentRequestUpdateEvent-> pause
